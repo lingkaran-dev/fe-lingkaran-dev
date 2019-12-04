4f0225730256baf58ad52d90348bd712ad46165f
Mahbub <prof.bub@live.com>
2019-12-04 10:42:30 +0700

    Updates

---
+++ precache-manifest.847f69884e5dd07f2881e59902dbdeec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a339930ab5e5588420d226149c8446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "694f2d661c5c3a046f5a",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5c442bb9638d3258eb21",
+    "url": "/static/js/2.fdcec7b3.chunk.js"
+  },
+  {
+    "revision": "694f2d661c5c3a046f5a",
+    "url": "/static/js/main.4647d116.chunk.js"
+  },
+  {
+    "revision": "7f34624c6b216cda8b17",
+    "url": "/static/js/runtime-main.480952de.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
